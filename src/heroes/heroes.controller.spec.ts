87c035d1546a07b37d337c7ef8cd60e8346fe734
michaljarnot <m.jarnot@yahoo.com>
2022-01-18 16:27:28 +0100

    test: heroes controller

---
+++ src/heroes/heroes.controller.spec.ts
@@ -0,0 +1,22 @@
+import { HeroesGameController } from './heroes.controller';
+import { Test } from '@nestjs/testing';
+import { HeroesGameModule } from './heroes.module';
+
+describe('HeroesGameController', () => {
+  let controllerToTest: HeroesGameController;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [HeroesGameModule],
+    }).compile();
+
+    controllerToTest =
+      moduleRef.get<HeroesGameController>(HeroesGameController);
+  });
+
+  describe('findAll', () => {
+    it('should return an array of cats', async () => {
+      expect(await controllerToTest.findAll()).toBe([]);
+    });
+  });
+});
